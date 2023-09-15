15605ea623d4fac0c0a317f74678d3a00b443630
zypayev <ramazan.zypayev@nu.edu.kz>
2023-09-15 14:18:38 +0600

    Initial Commit

---
+++ CardGame/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  CardGame
+//
+//  Created by Ramazan Zypayev on 9/15/23.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
